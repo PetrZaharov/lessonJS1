a230bcb527b6e7c74b722d7bd50b6725512d82de
Zaharov <PZaharov64@gmail.com>
2019-07-26 15:28:47 +0400

    Добавил задачку с разными типами данных

--- lessonJS1/lesson1.js
+++ lessonJS1/lesson1.js
@@ -7,3 +7,7 @@ let name = prompt('what is your name?');
 let admin = name;
 alert('admin');
 
+let a = 1000 + '108';
+console.log(a);
+//результат будет 1000108, т.к. 108 имеет строковое значение, а следовательно просто склеит строку
+
