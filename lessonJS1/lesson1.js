607516dcf2c68542e57cb591a82b4fa4e6fb54f5
Zaharov <PZaharov64@gmail.com>
2019-07-26 15:27:00 +0400

    Добавил задачку с заменой переменных

--- lessonJS1/lesson1.js
+++ lessonJS1/lesson1.js
@@ -2,3 +2,8 @@
 let celT = +prompt('Задайте температуру, которую вы хотите конвертировать');
 let farT = (9 / 5) * celT + 32;
 alert(`В фарентгейтах это будфет ${farT} градусов`);
+
+let name = prompt('what is your name?');
+let admin = name;
+alert('admin');
+
